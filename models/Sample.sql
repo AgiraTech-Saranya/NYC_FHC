e02b13b824c6f2d1c4dd5b7db10d39e9cbb4b457
Saranya_Agira <saranya.b@agiratech.com>
2023-07-18 18:29:47 +0530

    yml files modified

--- models/Sample.sql
+++ models/Sample.sql
@@ -3,7 +3,7 @@ select * from `dbtcore_project.Crash_Report`
 ),
 final as
 (
-select current_date() as date, crash_date
+select current_date()  as date, crash_date
 from sample
 )
 select * from final; --this is the sample file
